6d2d7ddcacd1460d3a279acb01553c0d75a003af
Celtic-Trinity-Studios <150298771+Celtic-Trinity-Studios@users.noreply.github.com>
2025-07-09 17:20:48 -0500

    Initial Commit

---
+++ Source/FaldoranPrime/FaldoranPrime.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FaldoranPrime.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FaldoranPrime, "FaldoranPrime" );
